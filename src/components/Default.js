b979b0511b8c08c8274e7193b29896f389d227f6
GohYH <yewhoegoh@yahoo.com.sg>
2021-06-29 20:59:18 +0800

    Default.js added msg

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,9 +3,28 @@ import React, { Component } from 'react'
 export default class Default extends Component {
     render() {
         return (
-            <div>
-                <h3>Page not found</h3>
+            <div style={pageContentContainer}>
+                <h3 style={pageContent}> 
+                    <span style={textContent}>
+                        Page not found
+                    </span> 
+                         - Please click the <span style={textContent}>Menu</span> for Home Page
+                </h3>
             </div>
         )
     }
+}
+
+const pageContentContainer = {
+    marginTop: '50px',
+    
+}
+const pageContent = {
+    textAlign:'center',
+    paddingBottom:'50px',
+    wordSpacing: '6px',
+}
+
+const textContent={
+    fontWeight: 'bold',
 }
